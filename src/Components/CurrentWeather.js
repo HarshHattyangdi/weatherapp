7077938ede9b2a9acd96ec5e335fe5c78e1519f4
Harshal <harshal.2797@gmail.com>
2021-12-26 16:39:18 +0530

    Updated request frequency to 1

--- src/Components/CurrentWeather.js
+++ src/Components/CurrentWeather.js
@@ -4,77 +4,27 @@ function CurrentWeather() {
   const KEY = process.env.REACT_APP_API_KEY;
   const BASE_URL = process.env.REACT_APP_BASE_URL;
 
-  const [lat, setLat] = useState(null);
-  const [long, setLong] = useState(null);
-  const [city, setCity] = useState(null);
-  const [cityInput, setCityInput] = useState(city);
+  const [city, setCity] = useState("Mumbai");
   const [data, setData] = useState(null);
 
   useEffect(() => {
-    navigator.geolocation.getCurrentPosition((position) => {
-      setLat(position.coords.latitude);
-      setLong(position.coords.longitude);
-    });
-
-    let finalEndpointCoord = `${BASE_URL}lat=${lat}&lon=${long}&units=metric&appid=${KEY}`;
-    console.log(BASE_URL);
-    fetch(finalEndpointCoord)
+    const generateRequestString = () => {
+      let generatedString = BASE_URL + `q=${city}&appid=${KEY}`;
+      return generatedString;
+    };
+    fetch(generateRequestString())
       .then((res) => res.json())
-      .then((result) => {
-        setData(result.main);
-        setCity(result.name);
-        console.log(city, data);
-      })
-      .catch((err) => console.log(err));
-    // axios
-    //   .get(finalEndpoint)
-    //   .then((res) => console.log(res))
-    //   .catch((err) => console.log(err));
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [lat, long]);
-
-  const getData = (e) => {
-    e.preventDefault();
-    let finalEndpointCity = `${BASE_URL}q=${city}&units=metric&appid=${KEY}`;
-    console.log(finalEndpointCity);
-
-    fetch(finalEndpointCity)
-      .then((res) => res.json())
-      .then((result) => {
-        setData(result.main);
-        setCity(cityInput);
+      .then((data) => {
         console.log(data);
+        setData(data);
       })
       .catch((err) => console.log(err));
-  };
+  }, [KEY, BASE_URL, city]); //This hook is only run when the value of city is updated, since KEY and BASEURL remain constants they can be removed as well from the dependency array
+
   return (
     <div>
-      <form onSubmit={(e) => getData(e)}>
-        <input
-          type="text"
-          name=""
-          id=""
-          placeholder="Enter City"
-          required
-          onChange={(e) => {
-            setCityInput(e.target.value);
-          }}
-        />
-        <input type="submit" value="Get Details" />
-      </form>
-      {data && city && (
-        <div>
-          <h2>{city.toUpperCase()}</h2>
-          {Object.keys(data).map((key, i) => (
-            <li key={i}>
-              <span>
-                {key} : {data[key]}
-              </span>
-            </li>
-          ))}
-        </div>
-      )}
-      {/* {data && data.feels_like} */}
+      <h3>Current Weather of {city.toUpperCase()}</h3>
+      {<pre>{JSON.stringify(data, null, 2)}</pre>}
     </div>
   );
 }
