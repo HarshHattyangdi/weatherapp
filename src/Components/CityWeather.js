cf9949bb58060bef609fab209ebec6aaa6c1b80a
HarshalHattyangdi <harshal.2797@gmail.com>
2022-01-01 20:23:04 +0530

    Empty commit to test upstream

--- src/Components/CityWeather.js
+++ src/Components/CityWeather.js
@@ -12,6 +12,7 @@ const CityWeather = (props) => {
           return (
             <li key={main[el] + idx}>
               {el}:{main[el]}
+              Test
             </li>
           );
         })}
